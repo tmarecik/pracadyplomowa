3ee3ddb849e7e08c6c0e95b9bbfc8d6e54c3b72e
Tomi79 <marecik@op.pl>
2020-05-05 23:08:25 +0200

    poprawiono test dao, dodano test findALl

--- src/test/java/pl/edu/wszib/pracadyplomowa/dao/dbtest.java
+++ src/test/java/pl/edu/wszib/pracadyplomowa/dao/dbtest.java
@@ -9,6 +9,9 @@ import pl.edu.wszib.pracadyplomowa.model.Product;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,20 +19,31 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class dbtest {
 
+    public final byte[] picture = Files.readAllBytes(Paths.get("C:\\Users\\Tomek\\JAVA\\pracadyplomowa\\src\\test\\java\\pl\\edu\\wszib\\pracadyplomowa\\under_construction.png"));
+    public final byte[] icon = Files.readAllBytes(Paths.get("C:\\Users\\Tomek\\JAVA\\pracadyplomowa\\src\\test\\java\\pl\\edu\\wszib\\pracadyplomowa\\under_construction.png"));
+    public static final int COUNTER = 2;
+
     @Autowired
     ProductDao productDao;
 
-    @BeforeEach
-    void setUp() throws IOException {
+    public dbtest() throws IOException {
+    }
 
-        byte[] picture = Files.readAllBytes(Paths.get("C:\\Users\\Tomek\\JAVA\\pracadyplomowa\\src\\test\\java\\pl\\edu\\wszib\\pracadyplomowa\\under_construction.png"));
-        byte[] icon = Files.readAllBytes(Paths.get("C:\\Users\\Tomek\\JAVA\\pracadyplomowa\\src\\test\\java\\pl\\edu\\wszib\\pracadyplomowa\\under_construction.png"));
+    @BeforeEach
+    void setUp(){
+
         Product product = new Product("gitara", picture, icon, "ala m kota", 112, 1 );
-        System.out.println("----============== PRODUCT ===========----------   "+ product);
+        Product product1 = new Product("gitara1", picture, icon, "ala m kota", 111, 0 );
 
         assertNull(product.getId());
         productDao.save(product);
+        productDao.save(product1);
         assertNotNull(product.getId());
+
+        List productDB = productDao.findAll();
+        for (Object prod: productDB) {
+            System.out.println(prod.getClass().getName());
+        }
     }
 
     @AfterEach
@@ -39,9 +53,16 @@ public class dbtest {
 
     @Test
     void testFetchProduct() {
-        Product productDB = productDao.getById(1L);
-//        Product productDB = productDao.findById(1L);
+        Product productDB = productDao.getById(2L);
         assertNotNull(productDB);
         assertEquals(productDB.getId(), productDB.getId());
     }
+
+    @Test
+    void testFetchAllProduct() {
+        List productDB = productDao.findAll();
+        assertNotNull(productDB);
+        assertEquals(productDB.size(),2);
+    }
+
 }
