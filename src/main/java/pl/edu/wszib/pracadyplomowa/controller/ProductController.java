07eb4e45bfbaec5d4a5b6f000762ccbf95d2122a
Tomi79 <marecik@op.pl>
2020-05-27 13:57:24 +0200

    welcome page

--- src/main/java/pl/edu/wszib/pracadyplomowa/controller/ProductController.java
+++ src/main/java/pl/edu/wszib/pracadyplomowa/controller/ProductController.java
@@ -35,17 +35,6 @@ public class ProductController {
         return "welcome";
     }
 
-    @GetMapping("shipping")
-    public String shipping(Model model){      //umożliwia przekazywanie danych do nowego szablonu
-        model.addAttribute("shippingMethodName1", "Paczkomat");             //dwefiniuje pola i wartości w szablonie shipping!!!! do których bedziemy sie odwoływać
-        model.addAttribute("shippingMethodName2", "Kurier -przedpłata");
-        model.addAttribute("shippingMethodName3", "Kurier -pobranie");
-        model.addAttribute("shippingMethodPrice1", 10.50);
-        model.addAttribute("shippingMethodPrice2", 15.00);
-        model.addAttribute("shippingMethodPrice3", 20.20);
-        return "shipping";
-    }
-
     @GetMapping("/products")
     public String products(Model model){
         detailsService.clearDetailsMap();
